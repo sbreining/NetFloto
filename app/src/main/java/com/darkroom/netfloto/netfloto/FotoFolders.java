e21bad55645f29db9dc72861f9c545d4e7dfac1c
Shane <sfbreining@gmail.com>
2017-09-14 13:47:29 -0700

    Project Started

---
+++ app/src/main/java/com/darkroom/netfloto/netfloto/FotoFolders.java
@@ -0,0 +1,13 @@
+package com.darkroom.netfloto.netfloto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FotoFolders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto_folders);
+    }
+}
